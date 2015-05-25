aaac1f8dc9f11be3ab6d52f3392029725bed04d8
lshapton <shaptonleanne@gmail.com>
2015-05-25 17:23:41 -0400

    delete files from git

--- db/migrate/20150517231247_add_fitbit_oauth_fields_to_patients.rb
+++
@@ -1,6 +0,0 @@
-class AddFitbitOauthFieldsToPatients < ActiveRecord::Migration
-  def change
-  	add_column :patients, :fitbit_oauth_token, :string
-  	add_column :patients, :fitbit_oauth_secret, :string
-  end
-end
