89a3f44055acb97f82b2550fa003f30514546947
lshapton <shaptonleanne@gmail.com>
2015-05-25 22:56:18 -0400

    remove migration for goal_id_id

--- db/migrate/20150525225452_create_asthma_apps.rb
+++ db/migrate/20150525225452_create_asthma_apps.rb
@@ -1,7 +1,6 @@
 class CreateAsthmaApps < ActiveRecord::Migration
   def change
     create_table :asthma_apps do |t|
-      t.references :goal_id, index: true, foreign_key: true
       t.integer :rescueInhaler_1
       t.integer :rescueInhaler_2
 
